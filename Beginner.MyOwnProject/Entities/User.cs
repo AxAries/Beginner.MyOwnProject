43a2ea383b1da97d5e43cb0b4787212e8b80e1da
dinodawid <93006994+dinodawid@users.noreply.github.com>
2022-02-02 13:49:23 +0100

    Komit 3

---
+++ Beginner.MyOwnProject/Entities/User.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Beginner.MyOwnProject.Entities
+{
+    public class User
+    {
+        public int ID { get; set; }
+        public string name { get; set; }
+        public string surname { get; set; }
+        public string email { get; set; }
+        public int phone_number { get; set; }
+        public string? NIP {get; set;}
+        public string passwordHash { get; set; }
+        public int roleID { get; set; }
+        public virtual UserRole UserRole { get; set; }
+
+    }
+}
